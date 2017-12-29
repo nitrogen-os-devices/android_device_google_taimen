a5ce817277782ecd9fa46803c0ca1c793dc50b54
nitin1438 <nitin.chobhe@gmail.com>
2017-12-29 18:46:33 +0530

    taimen: changes for Nitrogen OS

--- nitrogen_taimen.mk
+++ nitrogen_taimen.mk
@@ -4,10 +4,10 @@ TARGET_SCREEN_WIDTH := 1440
 
 
 # Inherit Carbon GSM telephony parts
-$(call inherit-product, vendor/carbon/config/gsm.mk)
+$(call inherit-product, vendor/nitrogen/products/common.mk)
 
 # Inherit Carbon product configuration
-$(call inherit-product, vendor/carbon/config/common.mk)
+#$(call inherit-product, vendor/carbon/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/taimen/aosp_taimen.mk)
@@ -15,7 +15,7 @@ $(call inherit-product, device/google/taimen/aosp_taimen.mk)
 -include device/google/taimen/device-carbon.mk
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := carbon_taimen
+PRODUCT_NAME := nitrogen_taimen
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 2 XL
 TARGET_MANUFACTURER := LGE
@@ -26,7 +26,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT=google/taimen/taimen:8.1.0/OPM1.171019.018/4518263:user/release-keys \
     PRIVATE_BUILD_DESC="taimen-user 8.1.0 OPM1.171019.018 4518263 release-keys"
 
-PRODUCT_PROPERTY_OVERRIDES += \
+#PRODUCT_PROPERTY_OVERRIDES += \
     ro.carbon.maintainer="frap129/Myself5"
 
 $(call inherit-product-if-exists, vendor/google/taimen/taimen-vendor.mk)
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 #
-# Copyright 2017 The Android Open Source Project
+# Copyright 2012 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,6 +14,4 @@
 # limitations under the License.
 #
 
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_taimen.mk \
-    $(LOCAL_DIR)/nitrogen_taimen.mk
+add_lunch_combo nitrogen_taimen-userdebug
